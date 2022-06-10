795dfe214e3393b5cb00189b370d804a948771bb
rkaufman <rkaufman@student.42wolfsburg.de>
2022-06-10 14:40:32 +0200

    fixed mem leak in targetgenerator when adding already existing target to it

--- cpp_module_02/TargetGenerator.cpp
+++ cpp_module_02/TargetGenerator.cpp
@@ -6,7 +6,7 @@
 /*   By: rkaufman <rkaufman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 12:12:24 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/06/08 12:42:22 by rkaufman         ###   ########.fr       */
+/*   Updated: 2022/06/10 14:39:38 by rkaufman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,11 @@ TargetGenerator::~TargetGenerator(void)
 void	TargetGenerator::learnTargetType(ATarget * target)
 {
 	if (target)
+	{
+		std::map<std::string, ATarget *>::iterator it = this->targets.find(target->getType());
+		if (it == targets.end())
 		this->targets[target->getType()] = target->clone();
+	}
 }
 
 void	TargetGenerator::forgetTargetType(std::string const & targetName)
